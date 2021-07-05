ad2b324c2129c8c574fda258532d58adaf762cbb
Alejandro Flores <aleksgnu@gmail.com>
2021-07-05 00:16:45 -0500

    feat(create user): simple post create user.

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,11 +1,23 @@
 # Rest Framework
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 # Models
 from api.users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[
+            UniqueValidator(queryset=User.objects.all())
+        ]
+    )
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+        ]
